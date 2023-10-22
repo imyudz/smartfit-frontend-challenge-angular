c6437317cf5634f4a300943ed66f75df35cd32e1
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-10-22 03:24:55 -0300

    feat: add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
-  results = []
-  
+  results = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+  }
+
+  onClean() {
+    this.formGroup.reset();
+  }
+
 }
